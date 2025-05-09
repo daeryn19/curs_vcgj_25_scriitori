700f2499969a3b09690aac01ef1296d3a189e9c9
daria-ghimus <dariaghimus02@gmail.com>
2025-05-09 21:37:41 +0000

    Adăugat fișierul de test pentru feature

---
+++ app/tests/test_atribute.py
@@ -0,0 +1,7 @@
+import libs.atribute as atribute
+
+def test_opera_reprezentativa():
+    assert atribute.gaseste_opera_reprezentativa() == "Secretul celor trei"
+
+def test_curent_literar():
+    assert atribute.gaseste_curent_literar() == "Thriller și Suspans"

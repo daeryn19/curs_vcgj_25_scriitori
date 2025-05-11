2299485c8d5bc89f7e9e5c47aa8cbfd28839ce0c
RaduCazanaru <ioan_radu.cazanaru@stud.etti.upb.ro>
2025-05-11 13:02:44 +0300

    added lib

---
+++ lib/biblioteca_scriitori.py
@@ -0,0 +1,16 @@
+def data_nasterii():
+    return "8 octombrie 1920"
+
+def data_mortii():
+    return "11 februarie 1986"
+
+def opere():
+    return [
+        "Dune",
+        "Dune Messiah",
+        "Children of Dune",
+        "God Emperor of Dune",
+        "Heretics of Dune",
+        "Chapterhouse: Dune"
+    ]
+

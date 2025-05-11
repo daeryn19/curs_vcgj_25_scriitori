4ae786f1cfef0c324dec00d2f529dd8ed98959a0
Elena-g41 <gurambaelena@gmail.com>
2025-05-11 12:30:19 +0000

    Adaugare folder lib

---
+++ app/lib/biblioteca_scriitori.py
@@ -0,0 +1,5 @@
+def descriere_mihail_sadoveanu():
+    return "Mihail Sadoveanu a fost un important scriitor român, cunoscut pentru operele sale inspirate din istorie și viața rurală."
+
+def opere_mihail_sadoveanu():
+    return "Frații Jderi, Baltagul, Hanu Ancuței, Neamul Șoimăreștilor"

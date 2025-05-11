a26fcd1870be3c5d48ea3f85de2c3b8136d94c7e
Cristian-Iustinian <cristianmarin1156@gmail.com>
2025-05-11 12:57:03 +0000

    Adauga folderul app in branch

---
+++ app/tests/test_biblioteca_scriitori.py
@@ -0,0 +1,18 @@
+import lib.biblioteca_scriitori as b_scriitori
+
+def test_viata_eminescu():
+	v = b_scriitori.viata_eminescu()
+	if b == "frumoasa":
+		assert True
+	else:
+		assert False
+
+
+def test_opera_eminescu():
+	o = b_scriitori.opera_eminescu()
+	if o == "Luceafarul":
+		assert True
+	else:
+		assert False	
+			
+	

2cc55116770e7c8118af575caba5ad4fa0476c56
daria-ghimus <dariaghimus02@gmail.com>
2025-05-12 00:25:54 +0300

    Revert "Integrare finală proiect - Ghimus Daria"

--- app/libs/atribute.py
+++
@@ -1,9 +0,0 @@
-"Acest modul conține funcții care returnează informații despre operele literare."
-
-def gaseste_opera_reprezentativa():
-    "Returneaza numele operei reprezentative"
-    return "Secretul celor trei"
-
-def gaseste_curent_literar():
-    "Returneaza curentul literar asociat"
-    return "Thriller și Suspans"
